5116cc570e91a548e4ca34650d52f10796fc4698
alexmadu <ogochukwumadu.ng@gmail.com>
2023-03-02 09:27:37 +0100

    updated media queries

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -67,15 +67,17 @@ const Work = () => {
                 <img src={urlFor(work.imgUrl)} alt={work.name} />
 
                 <motion.div
-                  whileHover={{ opacity: [0, 1] }}
-                  transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                  whileInView={{ opacity: 1 }}
+                  // whileHover={{ opacity: [0, 1] }}
+                  // transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                   className='app__work-hover app__flex'
                 >
                   <a href={work.projectLink} target="_blank" rel="noopener noreferrer">
                     <motion.div
-                      whileInView={{ scale: [0, 1] }}
-                      whileHover={{ scale: [1, 0.9] }}
-                      transition={{ duration: 0.25}}
+                      whileInView={{ opacity: 1 }}
+                      // whileInView={{ scale: [0, 1] }}
+                      // whileHover={{ scale: [1, 0.9] }}
+                      // transition={{ duration: 0.25}}
                       className='app__flex'
                     >
                       <AiFillEye />
@@ -85,12 +87,13 @@ const Work = () => {
                   <a href={work.codeLink} target="_blank" rel="noopener noreferrer">
                     <motion.div
                       whileInView={{ scale: [0, 1] }}
-                      whileHover={{ scale: [1, 0.9] }}
-                      transition={{ duration: 0.25}}
+                      // whileHover={{ scale: [1, 0.9] }}
+                      // transition={{ duration: 0.25}}
                       className='app__flex'
                     >
                       <AiFillGithub />
                     </motion.div>
+           
                   </a>
                 </motion.div>
               </div>
